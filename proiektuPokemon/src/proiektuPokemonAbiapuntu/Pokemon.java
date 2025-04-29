887a5e7601ff601b2cc1b68311b22091020f8748
MAlonsoF <marcoalonsofernandez@gmail.com>
2025-04-29 20:07:25 +0200

    Update Pokemon.java

--- proiektuPokemon/src/proiektuPokemonAbiapuntu/Pokemon.java
+++ proiektuPokemon/src/proiektuPokemonAbiapuntu/Pokemon.java
@@ -21,7 +21,7 @@ public abstract class Pokemon {
     public void erasotu(Pokemon etsaia, Ekintza ekintza) {
         System.out.println(this.izena + " erabiltzen ari da " + ekintza.getIzena() + " " + etsaia.izena + "ren aurka.");
 
-        double probabilitatea = Math.random(); // entre 0.0 y 1.0
+        double probabilitatea = Math.random(); //  0.0 eta 1.0 artean
         if (probabilitatea < 0.15) {
             System.out.println(ekintza.getIzena() + " huts egin du!");
             return;
@@ -29,9 +29,9 @@ public abstract class Pokemon {
 
         double kaltea = kalkulatuKaltea(ekintza, etsaia);
 
-        //  Golpe crítico: 10% de probabilidad
+        //  Kritikoa: 10%-ko probabilitatea
         if (Math.random() < 0.1) {
-            kaltea *= 2;
+            kaltea = kaltea*2;
             System.out.println("KOLPE KRITIKOA! Kaltea bikoiztuta!");
         }
 
@@ -51,7 +51,12 @@ public abstract class Pokemon {
     }
 
     public boolean bizirikDago() {
-        return bizitza > 0;
+        if (bizitza > 0) {
+        	return true; 
+        }
+        else {
+        	return false;
+        }
     }
 
     protected EkintzaZerrenda getEkintzak() {
